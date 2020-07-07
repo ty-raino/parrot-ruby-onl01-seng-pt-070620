f6b2e194733ae1dca624e5f697d52a8570e26ce0
Ty Raino 👑 <mstymetris@yahoo.com>
2020-07-07 22:40:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot 
-  puts "Squawk!"
-  return parrot
+def parrot(phrase="Squawk")
+  puts phrase
+  return phrase
 end
 
